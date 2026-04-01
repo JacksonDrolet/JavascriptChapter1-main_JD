7686cffc77e278010ff866957f00e17dd0c668a1
JacksonDrolet <droletjj@oldcolony.info>
2026-04-01 12:10:37 -0400

    CHECK

---
+++ HandsOn2/project01-02.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Jackson Drolet
+    Date:   3/13/2026
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+service1Name = "Basic";
+service2Name = "Express";
+service3Name = "Extreme";
+service4Name = "Ultimate";
+service1Speed = "50 Mbsp";
+service2Speed = "100 Mbsp";
+service3Speed = "500 Mbsp";
+service4Speed = "1 Gig";
